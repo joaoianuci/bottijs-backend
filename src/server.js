3455fa6e3d4f02fee4ef0847bb8b6cb7d338a89b
João Ianuci <joaoianuci@gmail.com>
2019-11-10 21:21:42 -0300

    Configura conexão com o mongo fora do heroku

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const cors = require('cors');
 const app = express();
 
-mongoose.connect(process.env.MONGO_URL,{
+mongoose.connect(process.env.MONGO_URL || 'mongodb+srv://bottistore:bottistore@cluster0-xjfpv.mongodb.net/test?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
